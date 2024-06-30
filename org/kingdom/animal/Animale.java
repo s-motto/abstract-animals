7e34c5875b0e154868d591473e0e1f0e3ee75a39
s-motto <brinamotto@gmail.com>
2024-06-30 19:30:43 +0200

    added class animale

---
+++ org/kingdom/animal/Animale.java
@@ -0,0 +1,12 @@
+package org.kingdom.animal;
+
+public abstract class Animale {
+
+	public String dormi() {
+		return "Zzzz";
+	}
+	
+	public abstract String verso();	
+	
+	public abstract String mangia();
+}
